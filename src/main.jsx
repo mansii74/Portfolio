9c07cd88bb0ff351fcdb1cf536fb43235393a71f
Mansi <mansi01kri@gmail.com>
2025-04-23 14:03:54 +0530

    package editing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 👈 Import BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Portfolio"> {/* 👈 Add basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
